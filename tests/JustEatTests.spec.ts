a750026b679ccf3ee830d2ee707b33943e229d00
jimmy-fly-qe <jamesdeclonflynn@hotmail.com>
2026-06-17 20:41:58 +0100

    Remove unused name import and fix indentation

--- tests/JustEatTests.spec.ts
+++ tests/JustEatTests.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '../src/fixtures/fixtures';
-import { BasketPage } from '../src/pages/BasketPage';
 import { ExcelReader } from '../src/utils/ExcelReader';
 
 const testData = ExcelReader.getSheetData('Sheet1', './test-data/TestData.xlsx');
@@ -8,34 +7,34 @@ test.describe('Just Eat - End to End Order Flow', () => {
 
     for (const data of testData) {
 
-    test(`Verify location search functionality - Case: ${data.RestaurantID}`, async ({ homePage, page }) => {
-        await test.step(`When I search for location ${data.Postcode}`, async () => {
-            await homePage.searchPostcode(data.Postcode);
+        test(`Verify location search functionality - Case: ${data.RestaurantID}`, async ({ homePage, page }) => {
+            await test.step(`When I search for location ${data.Postcode}`, async () => {
+                await homePage.searchPostcode(data.Postcode);
+            });
+
+            await test.step(`Then I should see a list of restaurants delivering to ${data.SearchResultsTitle}`, async () => {
+                await expect(page).toHaveTitle(data.SearchResultsTitle);
+            });
         });
 
-        await test.step(`Then I should see a list of restaurants delivering to ${data.SearchResultsTitle}`, async () => {
-            await expect(page).toHaveTitle(data.SearchResultsTitle);
+        test(`Order Pizza and checkout basket at ${data.RestaurantName}`, async ({ homePage, loginPage, searchResultsPage, pizzaRestaurantPage, basketPage, page }) => {
+            await test.step(`Given the user searches for restaurants in ${data.Postcode}`, async () => {
+                await homePage.searchPostcode(data.Postcode);
+            })
+
+            await test.step(`And access the chosen pizza restaurant ${data.RestaurantName}`, async () => {
+                await searchResultsPage.selectPizzaRestaurant(data.RestaurantName);
+            })
+
+            await test.step('When I add the items from the menu and confirm in the basket', async () => {
+                await pizzaRestaurantPage.addFoodToBasket();
+                await basketPage.checkOrderOptions();
+                await basketPage.goToCheckout();
+            })
+
+            await test.step(`Then I should see the login page before checkout possible`, async () => {
+                await expect(page).toHaveTitle(loginPage.pageTitle);
+            })
         });
-    });
-
-    test(`Order Pizza and checkout basket at ${data.RestaurantName}`, async ({ homePage, loginPage, searchResultsPage, pizzaRestaurantPage, basketPage, page }) => {
-        await test.step(`Given the user searches for restaurants in ${data.Postcode}`, async () => {
-            await homePage.searchPostcode(data.Postcode);
-        })
-
-        await test.step(`And access the chosen pizza restaurant ${data.RestaurantName}`, async () => {
-            await searchResultsPage.selectPizzaRestaurant(data.RestaurantName);
-        })
-
-        await test.step('When I add the items from the menu and confirm in the basket', async () => {
-            await pizzaRestaurantPage.addFoodToBasket();
-            await basketPage.checkOrderOptions();
-            await basketPage.goToCheckout();
-        })
-
-        await test.step(`Then I should see the login page before checkout possible`, async () => {
-            await expect(page).toHaveTitle(loginPage.pageTitle);
-        })
-    });
     }
 });
